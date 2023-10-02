0426e38b2860bcde6fcffcd8c26416960903bd53
Eugene <eugenetiron@yahoo.com>
2023-10-02 12:14:52 +0100

    Final Commit

---
+++ orders.js
@@ -0,0 +1,57 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'HD Camera Drone',
+        productNumber: '97352',
+        paymentStatus: 'Paid',
+        shipping: 'Shipped'
+    },
+    {
+        productName: 'Stealthy Drone',
+        productNumber: '65289',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Racing Drone',
+        productNumber: '12476',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Advanced Surveillance Drone',
+        productNumber: '38562',
+        paymentStatus: 'Paid',
+        shipping: 'Shipped'
+    },
+    {
+        productName: 'Explorer Drone',
+        productNumber: '78921',
+        paymentStatus: 'Due',
+        shipping: 'Processing'
+    },
+    {
+        productName: 'Mini Quadcopter',
+        productNumber: '57384',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Nano Drone',
+        productNumber: '93215',
+        paymentStatus: 'Paid',
+        shipping: 'Shipped'
+    },
+    {
+        productName: 'Aerial Photography Drone',
+        productNumber: '74691',
+        paymentStatus: 'Due',
+        shipping: 'Processing'
+    }
+
+]
